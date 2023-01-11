30e9299709442ed962d8c7787fc5ab39ab04feda
hrystynaa <yaskulskahrystyna@gmail.com>
2023-01-12 00:47:14 +0200

    Made configurations for app

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.[chunkhash].js',
+    path: path.resolve(__dirname, 'public')
+  },
+  devServer: {
+    port: 3000
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
