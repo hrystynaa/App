ef77a4ac8b4f3c1c93eb816030a8747fe855fad5
hrystynaa <yaskulskahrystyna@gmail.com>
2023-01-12 18:36:04 +0200

    Made render list

--- src/question.js
+++ src/question.js
@@ -1,15 +1,49 @@
 export class Question {
   static create(question) {
   return fetch('https://my-app-33d36-default-rtdb.firebaseio.com/questions.json', {
-        method: 'POST',
-        body: JSON.stringify(question),
-        headers: {
-            'Content-Type': 'application/json'
-        }
+      method: 'POST',
+      body: JSON.stringify(question),
+      headers: {
+          'Content-Type': 'application/json'
+      }
     })
       .then(response => response.json())
       .then(response => {
-        console.log(response);
+        question.id = response.name;
+        return question;
       })
+      .then(addToLocalStorage)
+      .then(Question.renderList);
   }
+
+  static renderList() {
+    const questions = getQuestionsFromLocalStorage();
+
+    const html = questions.length 
+    ? questions.map(toCard).join('')
+    : `<div class="mui--text-headline">Запитань ще не було</div>`;
+
+    const list = document.getElementById('list');
+    list.innerHTML = html;
+  }
+}
+
+function addToLocalStorage(question) {
+  const all = getQuestionsFromLocalStorage();
+  all.push(question);
+  localStorage.setItem('questions', JSON.stringify(all));
+}
+
+function getQuestionsFromLocalStorage() {
+  return JSON.parse(localStorage.getItem('questions') || '[]');
+}
+
+function toCard(question) {
+  return `
+  <div class="mui--text-black-54">
+    ${new Date(question.date).toLocaleDateString()}
+    ${new Date(question.date).toLocaleTimeString()}
+  </div>
+  <div>${question.text}</div>
+  `;
 }
