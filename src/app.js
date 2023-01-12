ef71ebc45e76315ad6f681b2b2119b05f26c9db8
hrystynaa <yaskulskahrystyna@gmail.com>
2023-01-12 16:14:54 +0200

    Add form code

--- src/app.js
+++ src/app.js
@@ -1,3 +1,29 @@
-import './styles.css'
+import { isValid } from './utils';
+import './styles.css';
 
-console.log('app working...')
+
+const form = document.getElementById('form');
+const input = form.querySelector('#question-input');
+const submitBtn = form.querySelector('#submit');
+
+form.addEventListener('submit', submitFormHandler);
+
+function submitFormHandler(event) {
+  event.preventDefault();
+
+  if (isValid(input.value)) {
+    const question = {
+        text: input.value.trim(),
+        date: new Date().toJSON(),
+    }
+
+    submitBtn.disabled = true;
+
+    console.log('Question', question);
+
+    input.value = '';
+    input.className = '';
+    submitBtn.disabled = false;
+  }
+  
+}
