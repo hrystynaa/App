018f01595303789ec4e6a61fe0c88b698f21e3ed
hrystynaa <yaskulskahrystyna@gmail.com>
2023-01-12 14:50:55 +0200

    Add something

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HTMLPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack=plugin');
 
 module.exports = {
   entry: './src/app.js',
@@ -13,6 +14,15 @@ module.exports = {
   plugins: [
     new HTMLPlugin({
       template: './src/index.html'
-    })
-  ]
+    }),
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
